2ddaf88f706c8b73ae35391cc28db02484a67b98
ryujimano <ryujimano@gmail.com>
2017-03-23 19:31:02 -0400

    add face to view

--- Canvas/ViewController.swift
+++ Canvas/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var trayView: UIView!
+    var trayOriginalCenter: CGPoint!
+    
+    var trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    
+    var newlyCreatedFace: UIImageView!
+    var newlyCreatedFaceOriginalCenter: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        trayDownOffset = 180
+        trayUp = trayView.center
+        trayDown = CGPoint(x: trayView.center.x, y: trayView.center.y + trayDownOffset)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,53 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            trayOriginalCenter = trayView.center
+        }
+        else if sender.state == .changed {
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: translation.y + trayOriginalCenter.y)
+        }
+        else if sender.state == .ended {
+            var velocity = sender.velocity(in: view)
+            if velocity.y > 0 {
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: [], animations: {
+                    self.trayView.center = self.trayDown
+                }, completion: nil)
+            }
+            else {
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: [], animations: {
+                    self.trayView.center = self.trayUp
+                }, completion: nil)
+            }
+        }
+    }
+    
+    @IBAction func createdFace(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        if sender.state == .began {
+            var faceView = sender.view as! UIImageView
+            newlyCreatedFace = UIImageView(image: faceView.image)
+            view.addSubview(newlyCreatedFace)
+            self.newlyCreatedFace.center = faceView.center
+            self.newlyCreatedFace.center.y += self.trayView.frame.origin.y
+            self.newlyCreatedFaceOriginalCenter = self.newlyCreatedFace.center
+            UIView.animate(withDuration: 0.1, animations: {
+                self.newlyCreatedFace.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            })
+        }
+        else if sender.state == .changed {
+            self.newlyCreatedFace.center = CGPoint(x: self.newlyCreatedFaceOriginalCenter.x + translation.x, y: self.newlyCreatedFaceOriginalCenter.y + translation.y)
+        }
+        else if sender.state == .ended {
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: [], animations: {
+                self.newlyCreatedFace.transform = self.newlyCreatedFace.transform.scaledBy(x: 0.7, y: 0.7)
+            }, completion: nil)
+        }
+    }
+    
 
 }
 
